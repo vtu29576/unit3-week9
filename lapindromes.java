494303c1553927e100d924edd91f95c43292a575
vtu29576 <vtu29576@veltech.edu.in>
2026-09-03 12:15:15 +0530

    Add files via upload

---
+++ lapindromes.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            String s = sc.next();
+            int n = s.length();
+
+            int[] count = new int[26];
+
+            // Left half
+            for (int i = 0; i < n / 2; i++) {
+                count[s.charAt(i) - 'a']++;
+            }
+
+            // Right half
+            for (int i = (n + 1) / 2; i < n; i++) {
+                count[s.charAt(i) - 'a']--;
+            }
+
+            boolean ok = true;
+
+            for (int x : count) {
+                if (x != 0) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            System.out.println(ok ? "YES" : "NO");
+        }
+    }
+}
